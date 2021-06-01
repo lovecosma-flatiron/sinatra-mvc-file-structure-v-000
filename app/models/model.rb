26977a6692b02cba91bad70ba2859354a986fba3
Matthew Williams <matthew.williams@flatironschool.com>
2021-05-31 19:38:52 -0600

    commit tree 1d72ecf421a19cb920c2951d5c791450a84caca5

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,11 @@
-class Model
+class Dog 
+
+    attr_accessor :name, :breed, :index
+
+    def initialize(name, breed, index)
+        @name = name
+        @breed = breed
+        @index = index
+    end 
 
 end
